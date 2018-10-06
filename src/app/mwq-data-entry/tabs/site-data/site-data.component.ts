a690e43a14aebb01a97bf616efea89ba2ee4704a
giriposhaneshwar <egp.designs@gmail.com>
2018-10-06 15:45:23 +0530

    Site Data form design completed

--- src/app/mwq-data-entry/tabs/site-data/site-data.component.ts
+++ src/app/mwq-data-entry/tabs/site-data/site-data.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'ms-site-data',
-  templateUrl: './site-data.component.html',
-  styleUrls: ['./site-data.component.scss']
+  selector: "ms-site-data",
+  templateUrl: "./site-data.component.html",
+  styleUrls: ["./site-data.component.scss"]
 })
 export class SiteDataComponent implements OnInit {
+  constructor(public route: Router) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  siteDateEdit() {
+    console.log("At Edit Screen");
   }
-
+  siteDateSave() {
+    console.log("At Save Screen");
+  }
+  siteDateNext() {
+    this.route.navigate(["mwqDataEntry", "in-situ-parameters"]);
+    console.log("At Next Screen");
+  }
+  ngOnInit() {}
 }
