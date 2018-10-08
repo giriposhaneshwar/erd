fb4af4bf90bbcfe1ce29aca6574013cf7050e86e
giriposhaneshwar <egp.designs@gmail.com>
2018-10-08 13:16:09 +0530

    situ table design in progress

--- src/app/mwq-data-entry/mwq-data-entry.module.ts
+++ src/app/mwq-data-entry/mwq-data-entry.module.ts
@@ -1,23 +1,23 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { MwqDataEntryRoutes } from './mwq-data-entry-routing.module';
-import { MwqDataEntryComponent } from './mwq-data-entry.component';
-import { RouterModule } from '@angular/router';
-import { NgxDatatableModule } from '@swimlane/ngx-datatable';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatInputModule, MatCardModule } from '@angular/material';
-import { DirectivesModule } from 'app/core/widgster/directives.module';
-import { SiteDataComponent } from './tabs/site-data/site-data.component';
-import { InSituParametersComponent } from './tabs/in-situ-parameters/in-situ-parameters.component';
-import { GeneralChemistryComponent } from './tabs/general-chemistry/general-chemistry.component';
-import { InOrganicChemistryComponent } from './tabs/in-organic-chemistry/in-organic-chemistry.component';
-import { OrganicChemistryComponent } from './tabs/organic-chemistry/organic-chemistry.component';
-import { MicroBiologyComponent } from './tabs/micro-biology/micro-biology.component';
-import { UploadFilesComponent } from './tabs/upload-files/upload-files.component';
-import { DemoMaterialModule } from '../material-demo.module';
-import { DataEntryComponent } from './tabs/data-entry/data-entry.component';
-
+import { MwqDataEntryRoutes } from "./mwq-data-entry-routing.module";
+import { MwqDataEntryComponent } from "./mwq-data-entry.component";
+import { RouterModule } from "@angular/router";
+import { NgxDatatableModule } from "@swimlane/ngx-datatable";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MatInputModule, MatCardModule } from "@angular/material";
+import { DirectivesModule } from "app/core/widgster/directives.module";
+import { SiteDataComponent } from "./tabs/site-data/site-data.component";
+import { InSituParametersComponent } from "./tabs/in-situ-parameters/in-situ-parameters.component";
+import { GeneralChemistryComponent } from "./tabs/general-chemistry/general-chemistry.component";
+import { InOrganicChemistryComponent } from "./tabs/in-organic-chemistry/in-organic-chemistry.component";
+import { OrganicChemistryComponent } from "./tabs/organic-chemistry/organic-chemistry.component";
+import { MicroBiologyComponent } from "./tabs/micro-biology/micro-biology.component";
+import { UploadFilesComponent } from "./tabs/upload-files/upload-files.component";
+import { DemoMaterialModule } from "../material-demo.module";
+import { DataEntryComponent } from "./tabs/data-entry/data-entry.component";
+import { CalendarComponent } from "../calendar/calendar.component";
 
 @NgModule({
   imports: [
@@ -25,12 +25,22 @@ import { DataEntryComponent } from './tabs/data-entry/data-entry.component';
     NgxDatatableModule,
     FormsModule,
     ReactiveFormsModule,
-    MatInputModule, 
-    MatCardModule, 
+    MatInputModule,
+    MatCardModule,
     DirectivesModule,
     DemoMaterialModule,
     RouterModule.forChild(MwqDataEntryRoutes)
   ],
-  declarations: [MwqDataEntryComponent, SiteDataComponent, InSituParametersComponent, GeneralChemistryComponent, InOrganicChemistryComponent, OrganicChemistryComponent, MicroBiologyComponent, UploadFilesComponent, DataEntryComponent]
+  declarations: [
+    MwqDataEntryComponent,
+    SiteDataComponent,
+    InSituParametersComponent,
+    GeneralChemistryComponent,
+    InOrganicChemistryComponent,
+    OrganicChemistryComponent,
+    MicroBiologyComponent,
+    UploadFilesComponent,
+    DataEntryComponent
+  ]
 })
-export class MwqDataEntryModule { }
+export class MwqDataEntryModule {}
--- src/app/mwq-data-entry/tabs/in-situ-parameters/in-situ-parameters.component.ts
+++ src/app/mwq-data-entry/tabs/in-situ-parameters/in-situ-parameters.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'ms-in-situ-parameters',
-  templateUrl: './in-situ-parameters.component.html',
-  styleUrls: ['./in-situ-parameters.component.scss']
+  selector: "ms-in-situ-parameters",
+  templateUrl: "./in-situ-parameters.component.html",
+  styleUrls: ["./in-situ-parameters.component.scss"]
 })
 export class InSituParametersComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
