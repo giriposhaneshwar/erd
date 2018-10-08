4264ee37fcd8b1a1b095e0e41e35db90621b2047
giriposhaneshwar <egp.designs@gmail.com>
2018-10-08 18:57:21 +0530

    situ form copleted

--- src/app/mwq-data-entry/tabs/in-situ-parameters/in-situ-parameters.component.ts
+++ src/app/mwq-data-entry/tabs/in-situ-parameters/in-situ-parameters.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "ms-in-situ-parameters",
@@ -6,7 +7,34 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./in-situ-parameters.component.scss"]
 })
 export class InSituParametersComponent implements OnInit {
-  constructor() {}
+  temperature: any = {
+    val1: 25,
+    val2: 65,
+    val3: 45,
+    val4: 15,
+    val5: 85
+  };
+  conductivity: any = {
+    val1: 25,
+    val2: 65,
+    val3: 75,
+    val4: 75,
+    val5: 2
+  };
+  constructor(public route: Router) {}
 
+  inputOrderClass(data, key) {
+    console.log("INput Data", data, key);
+  }
+  siteDatePrev() {
+    console.log("At Edit Screen");
+  }
+  siteDateSave() {
+    console.log("At Save Screen");
+  }
+  siteDateNext() {
+    this.route.navigate(["mwqDataEntry", "general-chemistry"]);
+    console.log("At Next Screen");
+  }
   ngOnInit() {}
 }
